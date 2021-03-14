d8341dd483fd0050719fcf53391504858ae762b1
greenmaskenergy <greenmaskenergy@gmail.com>
2021-03-14 10:29:44 +0100

    easter egg1

--- createModsListFile.bat
+++ createModsListFile.bat
@@ -1,3 +1,3 @@
 @echo off
-dir /b /a-d | findstr /v createModsListFile.bat |  findstr /v mods |  findstr /v update.bat > mods
+dir /b /a-d | findstr /v createModsListFile.bat |  findstr /v mods |  findstr /v update.bat |  findstr /v .gitignore> mods
 echo File: mods created
